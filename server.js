c931f3b8676905ca48cfae5ffe1fd4415d8f4ac5
Terverok <terverok@gmail.com>
2017-03-08 17:13:04 +0100

    kolejne poprawki

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ app.get('/', (req, res) => {
     return res.render('index')
 })
 
-//if middleware before didn't process given route we assume website don't exist
-//marek, 03.03.17r
 app.use('*', (req, res, next) => {
 	res.status(404)
 	
