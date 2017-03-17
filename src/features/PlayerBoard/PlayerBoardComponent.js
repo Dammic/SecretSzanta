359f4a7710d66c3ea9383ed57164eb651f168d91
Terverok <terverok@gmail.com>
2017-03-17 18:48:45 +0100

    Corrections for pull request

--- src/features/PlayerBoard/PlayerBoardComponent.js
+++ src/features/PlayerBoard/PlayerBoardComponent.js
@@ -2,9 +2,9 @@
 import React from 'react'
 
 const PlayerBoardComponent = ({
-left = [],
-middle = [],
-right = []
+    left = [],
+    middle = [],
+    right = []
 }) => {
 
     const renderPlayer = (player) => {
