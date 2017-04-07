5b91ad774eb1d7aa74abdf3ce27fcbc74fe5507c
Damian Michalski <dammic6988@gmail.com>
2017-04-07 18:55:49 +0200

    updated readme, increased build time a huge bit for a better development

--- src/vendors.js
+++ src/vendors.js
@@ -2,6 +2,12 @@
  * Inside this file, you need to add all vendor libraries you would like to use
  * on front. This will optimize build times, as we build vendor files separately
  */
+require('axios')
+require('bootstrap')
+require('lodash')
+require('moment')
 require('react')
+require('react-bootstrap')
 require('react-dom')
 require('react-router')
+require('socket.io-client')
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,6 @@
 'use strict'
-const webpack = require('webpack')
-const path = require('path')
-
-const BUILD_DIR = path.resolve(__dirname, 'public')
-const APP_DIR = path.resolve(__dirname, 'src')
+var path = require("path")
+var webpack = require("webpack")
 
 // hack for windows 10 ubuntu, ERROR in EINVAL: invalid argument, uv_interface_addresses
 // remove when MS fixes their WSL (now on windows version 1607)
@@ -15,44 +12,40 @@ try {
 
 module.exports = {
     cache: true,
-    devtool: 'source-map',
+    devtool: "eval",
     entry: {
-        app: APP_DIR + '/AppClient.js'
+        app: path.join(__dirname, "src", "AppClient.js")
     },
     watchOptions: {
         poll: true
     },
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: '[name].js',
-        chunkFilename: '[name].js'
+        path: path.join(__dirname, "public"),
+        filename: "[name].js",
+        chunkFilename: "[name].js"
     },
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false },
-            mangle: true,
-            beautify: false,
-            dead_code: true
-        }),
+        //Typically you'd have plenty of other plugins here as well
         new webpack.DllReferencePlugin({
-            context: path.join(__dirname, './'),
-            manifest: require('./vendor-manifest.json')
+            context: __dirname,
+            manifest: require("./vendor-manifest.json")
         })
     ],
     module: {
         loaders: [
             {
                 test: /\.js?$/,
-                loader: 'babel-loader',
+                loader: "babel",
                 include: [
-                    APP_DIR //important for performance!
+                    path.join(__dirname, "src") //important for performance!
                 ],
                 query: {
                     cacheDirectory: true, //important for performance
-                    presets: ['react', 'es2015', 'stage-3', 'stage-1']
+                    plugins: ["transform-regenerator"],
+                    presets: ["react", "es2015", "stage-3", "stage-1"]
                 }
             }, {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -68,14 +61,14 @@ module.exports = {
                 test: /\.scss$/,
                 loaders: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
                 include: [
-                    APP_DIR // important for performance!
+                    path.join(__dirname, "src") //important for performance!
                 ],
             }
         ]
     },
     resolve: {
-        extensions: ['', '.js', '.jsx'],
-        root: path.resolve(__dirname, './'),
-        modulesDirectories: ['node_modules']
+        extensions: ["", ".js"],
+        root: path.resolve(__dirname, "src"),
+        modulesDirectories: ["node_modules"]
     }
 }
