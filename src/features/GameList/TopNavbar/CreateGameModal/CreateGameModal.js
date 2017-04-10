773f0b59086cc9ec5820e86b4cd939f2c662ca72
Damian Michalski <dammic6988@gmail.com>
2017-04-10 21:43:10 +0200

    fixed pr issues

--- src/features/GameList/TopNavbar/CreateGameModal/CreateGameModal.js
+++ src/features/GameList/TopNavbar/CreateGameModal/CreateGameModal.js
@@ -3,10 +3,12 @@ import React from 'react'
 import CreateGameModalComponent from './CreateGameModalComponent'
 
 export default class CreateGameModal extends React.PureComponent {
+
     render () {
         const {showModal, onHide, onCreate, socket} = this.props
         return(
             <CreateGameModalComponent showModal={showModal} onHide={onHide} onCreate={onCreate} socket={socket}/>
+            
         )
     }
 }
