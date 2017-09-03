87686baf1d7eae1cb59c39267905a02e1817556b
Dammic <dammic6988@gmail.com>
2017-09-03 22:07:53 +0200

    small fix

--- src/ducks/roomDuck.js
+++ src/ducks/roomDuck.js
@@ -114,7 +114,6 @@ export default function reducer(state = initialState, action = {}) {
             return {
                 ...state,
                 playersDict: newPlayersDict,
-
                 votes: null,
             }
         }
