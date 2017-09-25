d745a2ca4e921cc7d7cdbf63dd7235e6253e5369
Marek Tkaczyk <terverok@gmail.com>
2017-09-25 21:48:50 +0200

    Now displays Names

--- src/features/Modal/WinningModal/WinningModalComponent.jsx
+++ src/features/Modal/WinningModal/WinningModalComponent.jsx
@@ -7,17 +7,20 @@ const WinningModalComponent = ({ winners, losers }) => {
         <div className="winning-modal">
             <div className="winning-modal-list">
                 {map(winners, winner => (
-                    <PlayerAvatarComponent
-                        key={winner.playerName}
-                        liberalAvatar={winner.avatarNumber}
-                        facistAvatar={winner.facistAvatar}
-                        className="winner-dance"
-                    />
+                    <div className="winner-dance">
+                        <div className="avatar-name">{winner.playerName}</div>
+                        <PlayerAvatarComponent
+                            key={winner.playerName}
+                            liberalAvatar={winner.avatarNumber}
+                            facistAvatar={winner.facistAvatar}
+                        />
+                    </div>
                 ))}
             </div>
             <div className="winning-modal-list">
                 {map(losers, loser => (
                     <div>
+                        <div className="avatar-name">{loser.playerName}</div>
                         <PlayerAvatarComponent
                             key={loser.playerName}
                             liberalAvatar={loser.avatarNumber}
