6e234045425deb9dd8841937a7324cb225f39a73
Marek Tkaczyk <terverok@gmail.com>
2017-12-21 07:50:08 +0100

    changed to fa-pause icon

--- src/features/Modal/HaltModal/HaltModalComponent.jsx
+++ src/features/Modal/HaltModal/HaltModalComponent.jsx
@@ -5,7 +5,7 @@ const HaltModalComponent = ({ hasGameEnded, ownerName }) => {
     const information = hasGameEnded ? "The game has ended!" : `The owner ${ownerName} paused the game.`
     return (
         <div className="halt-body">
-            <i className="fa fa-info-circle" aria-hidden="true" />
+            <i className="fa fa-pause" aria-hidden="true" />
             <span className="info">{information}</span>
         </div>
     )
