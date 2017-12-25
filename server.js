8a264758fa1423cab09bab42f375dffb2e033e7d
Dammic <dammic6988@gmail.com>
2017-12-25 19:24:00 +0100

    dll files are now much smaller (gzip)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const Server = require('http').Server
 const swig = require('swig')
 const Express = require('express')
+const expressStaticGzip = require('express-static-gzip');
 const SocketEvents = require('./events/SocketEvents')
 
 // initialize the server and configure support for ejs templates
@@ -16,21 +17,20 @@ const server = new Server(app)
 app.engine('html', swig.renderFile)
 app.set('view engine', 'html')
 app.set('views', path.join(__dirname, 'views'))
-app.use(Express.static(__dirname + '/public'))
 
 // socket.io
 const io = require('socket.io')(server)
 
 SocketEvents(io)
 
+app.use('/', expressStaticGzip(path.join(__dirname, '/public'), {
+    enableBrotli: false,
+}));
+
 // universal routing and rendering
 app.get('/', (req, res) => res.render('index'))
 
 app.use('*', (req, res, next) => {
-    res.status(404)
-
-    console.log('404! Not Found!')
-    console.log('original url:', req.originalUrl)
     return res.render('index')
 })
 
--- webpack.dll.js
+++ webpack.dll.js
@@ -1,28 +1,42 @@
 const path = require('path')
 const webpack = require('webpack')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
     entry: {
-        vendor: [path.join(__dirname, 'src', 'vendors.js')]
+        vendor: [path.join(__dirname, 'src', 'vendors.js')],
     },
     output: {
         path: path.join(__dirname, 'public', 'dll'),
         filename: 'dll.[name].js',
-        library: '[name]'
+        library: '[name]',
     },
     plugins: [
         new webpack.DllPlugin({
             path: path.join(__dirname, '[name]-manifest.json'),
             name: '[name]',
-            context: path.resolve(__dirname, 'src')
+            context: path.resolve(__dirname, 'src'),
         }),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
         new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.DefinePlugin({ // <-- key to reducing React's size
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compress: { warnings: false },
         }),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new CompressionPlugin({
+            asset: '[path].gz[query]',
+            algorithm: 'gzip',
+            regExp: /\.js$|\.css$|\.html$/,
+            threshold: 1,
+        }),
         new BundleAnalyzerPlugin({
-            analyzerMode: 'static'
-        })
+            analyzerMode: 'static',
+        }),
     ],
 }
