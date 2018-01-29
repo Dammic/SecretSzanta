1bd368ad788d70cfad52fe9c00368cad07968c41
Marek Tkaczyk <terverok@gmail.com>
2018-01-30 00:08:21 +0100

    logInfo: added universal logging

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,14 @@
+const moment = require('moment')
+
 const getCurrentTimestamp = () => {
     return Math.floor(Date.now() / 1000)
 }
 
+const logInfo = ({ currentRoom, currentPlayerName }, message) => {
+    console.log(`[${moment().format('MM-DD HH:mm')}] - INFO - [${currentRoom}][${currentPlayerName}]: ${message}`)
+}
+
 module.exports = {
-    getCurrentTimestamp
+    getCurrentTimestamp,
+    logInfo,
 }
