d27b8e408d3163aab69bd9c122b5278300726525
Marek Tkaczyk <terverok@gmail.com>
2018-02-05 12:09:32 +0100

    Shared/Button: classNames used

--- src/features/Shared/Button.jsx
+++ src/features/Shared/Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 export const Button = ({ label, onClick, className }) => {
+    const buttonClassNames = classNames('fabulous-button lined thick', className)
     return (
         <div
-            className={`${className || ''} fabulous-button lined thick`}
+            className={buttonClassNames}
             onClick={onClick}
             role="button"
             tabIndex="0"
