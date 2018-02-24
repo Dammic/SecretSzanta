4d24caaa96272390567be7ab327494fb9e95e716
dammic <dammic6988@gmail.com>
2018-02-24 20:38:23 +0100

    screw ie

--- webpack.dev.js
+++ webpack.dev.js
@@ -105,7 +105,7 @@ module.exports = {
                     presets: [
                         ['@babel/preset-env', {
                             targets: {
-                                browsers: ['last 2 versions', 'ie >= 7'],
+                                browsers: ['last 2 versions'],
                             },
                         }],
                     ],
