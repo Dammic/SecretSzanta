6b9e5747aaeec843bc618652e883032cdc9a86aa
Marek Tkaczyk <terverok@gmail.com>
2018-03-04 13:57:35 +0100

    GameEndModal: tweak how players are shown

--- src/features/Modal/GameEndModal/GameEndModal.jsx
+++ src/features/Modal/GameEndModal/GameEndModal.jsx
@@ -1,18 +1,20 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import GameEndModalComponent from './GameEndModalComponent'
+import { GameEndModalComponent } from './GameEndModalComponent'
 
 // eslint-disable-next-line
 class GameEndModal extends PureComponent {
     static propTypes = {
-        playerDict: PropTypes.objectOf(PropTypes.any),
+        playersDict: PropTypes.objectOf(PropTypes.any),
     }
-    render = () => (
-        <GameEndModalComponent
-            players={this.props.playerDict}
-        />
-    )
+    render() {
+        return (
+            <GameEndModalComponent
+                players={this.props.playersDict}
+            />
+        )
+    }
 }
 
 const mapStateToProps = ({ room }) => ({
