6e46820bd87044a3175a33668e30c71dcb3edd50
Marek Tkaczyk <terverok@gmail.com>
2018-03-18 17:59:42 +0100

    change used environment variable

--- utils/utils.js
+++ utils/utils.js
@@ -5,7 +5,7 @@ const { MessagesTypes } = require('../Dictionary')
 const getCurrentTimestamp = () => Math.floor(Date.now() / 1000)
 
 const log = ({ currentRoom, currentPlayerName }, messageType, message) => {
-    if (process.env.NODE_ENV === 'noConsole') return
+    if (process.env.NODE_SHOWCONSOLE === 'NO') return
     console.log(`[${moment().format('MM-DD HH:mm')}] - ${pad(messageType, 6)} - [${currentRoom}][${currentPlayerName}]: ${message}`)
 }
 
