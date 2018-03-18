ca96cf06fc0a384b0a9e6397047a27d67f31ec7e
Damian Michalski <dammic6988@gmail.com>
2018-03-18 18:30:02 +0100

    Add responsive header (mobile version header) (#122)
    * added mobile header version for landing page/lobby
    
    * moved color variable
    
    * fixed issue with mobile meta
    
    * fixed the contemplation question mark on modal close

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -6,22 +6,39 @@ import HeaderComponent from './HeaderComponent'
 
 export class Header extends React.PureComponent {
     static propTypes = {
+        setView: PropTypes.func.isRequired,
         currentView: PropTypes.string,
         userName: PropTypes.string,
     }
-    
+
+    state = {
+        isMobileNavOpen: false,
+    }
+
     setNewView = (newView) => {
         this.props.setView({ viewName: newView })
     }
 
+    openMobileNavigation = () => {
+        this.setState({ isMobileNavOpen: true })
+    }
+
+    closeMobileNavigation = () => {
+        this.setState({ isMobileNavOpen: false })
+    }
+
     render() {
         const { currentView, userName } = this.props
+        const { isMobileNavOpen } = this.state
         const shouldDisplayLobbyNav = (!!userName)
         return (
             <HeaderComponent
                 setView={this.setNewView}
                 currentView={currentView}
                 isLobbyVisible={shouldDisplayLobbyNav}
+                isMobileNavOpen={isMobileNavOpen}
+                openMobileNavigation={this.openMobileNavigation}
+                closeMobileNavigation={this.closeMobileNavigation}
             />
         )
     }
