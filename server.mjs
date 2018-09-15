a5feccfbdb502917c45a52f9c732f734f3c9d802
Damian Michalski <dammic6988@gmail.com>
2018-09-15 07:19:17 +0200

    feat(): commit hash is available in metatags (#189)

--- server.mjs
+++ server.mjs
@@ -1,24 +1,26 @@
 import path from 'path'
 import http from 'http'
 import Express from 'express'
+import ejs from 'ejs'
 import expressStaticGzip from 'express-static-gzip'
 import { initializeIO } from './io'
+import { getLatestGitCommitHash } from './utils/utils'
 
 // initialize the server and configure support for ejs templates
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 const app = new Express()
 const server = new http.Server(app)
+let gitVersionHash = ''
 
-// routes
+// view engine setup to ejs
+app.engine('html', ejs.renderFile)
+app.set('view engine', 'html')
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
 // socket.io
 initializeIO(server)
 
 app.get('/', (req, res) => {
-    return res.sendFile('index.html', { root: `${__dirname}/views` })
+    return res.render('index.html', { gitVersionHash })
 })
 
 app.use(expressStaticGzip(path.join(__dirname, '/public'), {
@@ -29,7 +31,7 @@ app.use(expressStaticGzip(path.join(__dirname, '/public'), {
 app.get('*', (req, res, next) => {
     res.status(404)
     console.log(`404! Page not found! Original url: ${req.originalUrl}`)
-    return res.sendFile('index.html', { root: `${__dirname}/views` })
+    return res.render('index.html', { gitVersionHash })
 })
 
 // start the server
@@ -40,4 +42,5 @@ server.listen(port, err => {
         return console.error(err)
     }
     console.info(`Server running on http://localhost:${port} [${env}]`)
+    gitVersionHash = getLatestGitCommitHash()
 })
