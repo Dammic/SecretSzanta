c2e4b9aa185a8f71e42fcd94858b01c6d0ea98d2
dammic <dammic6988@gmail.com>
2018-12-01 22:42:08 +0100

    fix for not working project

--- src/features/Notifications/StatusNotification.js
+++ src/features/Notifications/StatusNotification.js
@@ -3,14 +3,8 @@ import classNames from 'classnames/bind'
 import { markupMessage, replaceKeysWithComponent } from './utils'
 import styles from './Notifications.css'
 
-export class StatusNotification extends React.PureComponent {
-    render() {
-        const { message, values } = this.props
-
-        return (
-            <div className={classNames(styles.notificationPanel, styles.status)} >
-                {markupMessage(message, replaceKeysWithComponent(values))}
-            </div>
-        )
-    }
-}
+export const StatusNotification = ({ message, values }) => (
+    <div className={classNames(styles.notificationPanel, styles.status)}>
+        {markupMessage(message, replaceKeysWithComponent(values))}
+    </div>
+)
