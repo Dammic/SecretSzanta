e7b6203825da205a2e02f12ba961506ae3384bb2
dammic <dammic6988@gmail.com>
2018-12-16 14:13:20 +0100

    fixed font-awesome issue

--- webpack.config.js
+++ webpack.config.js
@@ -97,13 +97,8 @@ module.exports = (env, argv) => {
                                 ['@babel/react'],
                             ],
                         },
-                    }
+                    },
                 ],
-            }),
-            new HappyPack({
-                id: 'fonts',
-                threadPool: happyThreadPool,
-                loaders: ['url-loader'],
             }),
             new HappyPack({
                 id: 'css',
@@ -146,8 +141,10 @@ module.exports = (env, argv) => {
                 exclude: /node_modules/,
                 loader: 'happypack/loader?id=js',
             }, {
-                test: /\.(woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'happypack/loader?id=fonts',
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    'file-loader?limit=10000',
+                ],
             }, {
                 test: /\.(jpe?g|png|gif|svg)$/i,
                 use: imageLoaders,
