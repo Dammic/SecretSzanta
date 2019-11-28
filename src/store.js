adf6b16e3623442cb46da9b91a8665977867a9a4
dammic <dammic6988@gmail.com>
2019-11-28 10:31:15 +0100

    Bump node version to 13, update packages

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk'
-import createHistory from 'history/createBrowserHistory'
+import { createBrowserHistory } from 'history'
 import { rootReducer } from './ducks/rootDuck'
 
 /* eslint-disable no-underscore-dangle */
-export const history = createHistory()
+export const history = createBrowserHistory()
 const router = routerMiddleware(history)
 
 export const store = createStore(
