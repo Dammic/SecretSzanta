5a742ecb3d277c03bcfd3f985d4c04398fdf0713
dammic <dammic6988@gmail.com>
2019-11-29 08:19:22 +0100

    tests now run

--- src/config/test.config.js
+++ src/config/test.config.js
@@ -1,6 +1,6 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { socket } from './src/utils/SocketHandler'
+import { socket } from '../utils/SocketHandler'
 
 window.requestAnimationFrame = function (callback) {
     setTimeout(callback, 0)
